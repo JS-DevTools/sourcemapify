0ebf934db7f87432712fef4d6e87ab1cab74d616
James Messinger <james.messinger@gmail.com>
2016-12-24 13:43:30 -0600

    fixed a failing test due to NPM 2.0 installation directory

--- test/fixtures/paths.js
+++ test/fixtures/paths.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const fs = require('fs');
 const path = require('path');
 const paths = exports;
 
@@ -9,3 +10,12 @@ paths.dist = path.join(paths.root, 'dist');
 paths.entryFile = path.join(paths.lib, 'index.js');
 paths.bundleFile = path.join(paths.dist, 'bundle.js');
 paths.mapFile = path.join(paths.dist, 'bundle.js.map');
+
+if (fs.existsSync('node_modules/browser-pack')) {
+  // NPM 3.0 installs all dependencies directly under "node_modules"
+  paths.prelude = 'node_modules/browser-pack/_prelude.js';
+}
+else {
+  // NPM 2.0 installs dependencies under their parent module
+  paths.prelude = 'node_modules/browserify/node_modules/browser-pack/_prelude.js';
+}
